8a81df4f20e23495a11e9300ae18aca1b81794a2
Taha Şahin <115729027+tahasahinn@users.noreply.github.com>
2025-08-10 23:26:22 +0300

    Update auth.dto.js

--- auth/src/auth.dto.js
+++ auth/src/auth.dto.js
@@ -1,11 +1,11 @@
 const Joi = require("joi");
 
 const registerSchema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
-  firstName: Joi.string().required(),
-  lastName: Joi.string().required(),
-  role: Joi.string().valid("user", "admin").default("user"),
+  email: Joi.string().email().required(),s
+  password: Joi.string().min(6).required(),s
+  firstName: Joi.string().required(),s
+  lastName: Joi.string().required(),s
+  role: Joi.string().valid("user", "admin").default("user"),s
 });
 
 const loginSchema = Joi.object({
